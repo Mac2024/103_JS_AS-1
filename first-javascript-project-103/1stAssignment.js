c580a68ce90a623da763bc19ecb043c8f807960c
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-05-22 10:34:03 -0500

    Add files via upload

---
+++ first-javascript-project-103/1stAssignment.js
@@ -0,0 +1,22 @@
+// 20 variables assignment//
+let songName="song name";
+let songTitle="song title";
+let videogameName="Avengers";
+let videogameNew="2022 game";
+let userName="Johnny";
+let songGenre="country";
+let subscriptionGroup="My Group";
+let usermailAddress="john.c.gray@gmail.com";
+let accountId="My Id";
+let shopbyAge="30";
+let shopbyPrice="$20";
+let ageRange="20-25";
+let interestgenreToys="animals";
+let popularBooks="popular";
+let textbookTitle="title";
+let bookFormat="book format";
+let vehicleName="name of vehicle";
+let autoPrice="price";
+let petType="type of pet";
+let sellerName="seller name";
+document.writeln(`<p> ${songName},${songTitle}, ${videogameName}, ${userName}, ${songGenre}, ${subscriptionGroup}, ${usermailAddress}, ${accountId}, ${shopbyAge}, ${shopbyPrice}, ${ageRange}, ${interestgenreToys}, ${popularBooks}, ${textbookTitle}. ${bookFormat}, ${vehicleName}, ${autoPrice}, ${petType}, ${sellerName}</p>`);
---
+++ first-javascript-project-103/exercise2.js
@@ -0,0 +1,28 @@
+// 20 variables//
+let songName="song name";
+let songTitle="song title";
+let videogameName="name";
+let videogameNew="name";
+let userName="user name";
+let songGenre="genre";
+let subscriptionGroup="group name";
+let usermailAddress="mailing address";
+let accountId="id name";
+let shopbyAge="age";
+let shopbyPrice="price";
+let ageRange="age range";
+let interestgenreToys="interest genre";
+let popularBooks="popular books";
+let textbookTitle="title";
+let bookFormat="book format";
+let vehicleName="name of vehicle";
+let autoPrice="price";
+let petType="type of pet";
+let sellerName="seller name";
+
+document.writeln(`
+<p>${songName}</p>
+<p> Name: ${_name}</p>
+<p> Email: ${email}</p>
+<p> Salary: ${salary_yr}</p>
+`)
